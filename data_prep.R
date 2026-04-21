961f3bc08ec231f355b84419f09679fa9ad384d9
elisanoltenius <66408158+elisanoltenius@users.noreply.github.com>
2026-04-21 23:15:42 +0200

    Add files via upload

--- data_prep.R
+++ data_prep.R
@@ -32,44 +32,14 @@ length(intersect(df_equal$idunique, df_healthy$idunique))
 
 ## all the same participants
 common_cols = colnames(df_strong)[1:24]
-df = df_strong %>%
+
+# merge datasets
+df <- df_strong %>%
   left_join(df_equal[, c("idunique", "w4eq10")], by = "idunique") %>%
   left_join(df_healthy[, c("idunique", "w1hq8", "w3hq57")], by = "idunique")
 
-# only using data where we have info on "How long to find first job after starting active search"
-# --> variable w3sq69b must be category 1-4
-df = df[df$w3sq69b %in% 1:4,]
-
-df = df[df$age < 100, ]
-
-# use birthyear instead of age --> set first year in dataset to 0
-df$yrbrn0 = df$yrbrn - min(df$yrbrn)
-
-# recode w1hq8
-#' Value	Category
-#' 1	Yes, but I was unable to do so
-#' 2	Yes, and I was able to do so
-#' 3	No
-#' 9	No answer*
-df$w1hq8[df$w1hq8 %in% c(1,2)] = 1
-df$w1hq8[df$w1hq8 == 3] = 0
-df$w1hq8[df$w1hq8 == 9] = NA
-# Non informative categories set to NA
-df[df$agegroup35 == 9, "agegroup35"] = NA
-df$w3hq57[df$w3hq57 == 9] = NA
-df$w4sq1[df$w4sq1 == 9] = NA
-
-
-# recode to factors
-df$w4eq10[df$w4eq10 == 2] = 0
-df$w4eq10[df$w4eq10 == 9] = NA
-vars = c("w1sq6_1", "w1sq6_2", "w1sq6_3", "w1sq6_4", "agegroup35", "w1sq11_1",
-          "w1sq11_2", "w1sq11_3", "w1sq11_4", "w4eq10", "w1hq8", "w3hq57", "cntry")
-
-df = df %>%
-  mutate(across(all_of(vars), ~ factor(.)))
 # rename variables
-df_better <- df %>%
+df <- df %>%
   rename(
     timetojob = w3sq69b,
     during_studies_abroad_studies = w1sq6_1,
@@ -85,19 +55,69 @@ df_better <- df %>%
     mental_health_subj = w3hq57, # higher = bad
     inheritance = w4eq10
   )
-#
+
+# filter valid outcome
+df <- df[df$timetojob %in% 1:4, ]
+
+# clean age
+df <- df[df$age < 100, ]
+
+# birth year transformation
+df$yrbrn0 <- df$yrbrn - min(df$yrbrn, na.rm = TRUE)
+
+# recode psych_12month
+df$psych_12month[df$psych_12month %in% c(1,2)] <- 1
+df$psych_12month[df$psych_12month == 3] <- 0
+df$psych_12month[df$psych_12month == 9] <- NA
+
+# other missing recodes
+df[df$agegroup35 == 9, "agegroup35"] <- NA
+df$mental_health_subj[df$mental_health_subj == 9] <- NA
+df$workimportance_subj[df$workimportance_subj == 9] <- NA
+
+# recode inheritance
+df$inheritance[df$inheritance == 2] <- 0
+df$inheritance[df$inheritance == 9] <- NA
+
+# variables to convert to factor
+vars <- c(
+  "during_studies_abroad_studies",
+  "during_studies_abroad_work",
+  "during_studies_abroad_langcourse",
+  "during_studies_abroad_summerschool",
+  "agegroup35",
+  "apprent_unpaid",
+  "apprent_paid",
+  "intern_unpaid",
+  "intern_paid",
+  "inheritance",
+  "psych_12month",
+  "mental_health_subj",
+  "cntry"
+)
+
+df <- df %>%
+  mutate(across(all_of(vars), ~ factor(.)))
+
+# overview
+summary(df[, c("during_studies_abroad_studies", "during_studies_abroad_work",
+               "during_studies_abroad_langcourse", "during_studies_abroad_summerschool",
+               "agegroup35", "apprent_unpaid", "apprent_paid",
+               "intern_unpaid", "intern_paid", "inheritance", "psych_12month",
+               "mental_health_subj")])
+
 # Model ----
 #' EDUCATION
-#' w1sq6_1 - Done during studies: Studied abroad
-#' w1sq6_2 - Done during studies: Internship or work placement abroad
-#' w1sq6_3 - Done during studies: Attended language course abroad
-#' w1sq6_4 - Done during studies: Summer school, workshop or similar activity abroad
+#' w1sq6_1 - Done during studies: Studied abroad -> during_studies_abroad_studies
+#' w1sq6_2 - Done during studies: Internship or work placement abroad --> during_studies_abroad_work
+#' w1sq6_3 - Done during studies: Attended language course abroad --> during_studies_abroad_langcourse
+#' w1sq6_4 - Done during studies: Summer school, workshop or similar activity abroad --> during_studies_abroad_summerschool
 #'
 #' w1sq7_1 - Stay abroad supported by mobility programme: Yes, by EU programme (e.g. Erasmus)
 #' w1sq7_2 - Stay abroad supported by mobility programme: Yes, by national programme
 #' w1sq7_3 - Stay abroad supported by mobility programme: Yes, by another programme
 #'
-#' w1sq11_1 - Ever participated in apprenticeship or internship: Yes, in unpaid apprenticeship
+#' w1sq11_1 - Ever participated in apprenticeship or internship: Yes, in unpaid apprenticeship --> apprent_unpaid
 #' w1sq11_2 - Ever participated in apprenticeship or internship: Yes, in paid apprenticeship
 #' w1sq11_3 - Ever participated in apprenticeship or internship: Yes, in unpaid internship
 #' w1sq11_4 - Ever participated in apprenticeship or internship: Yes, in paid internship
@@ -130,7 +150,7 @@ model_cont = lm(timetojob ~ cntry +
                   intern_paid +
                   psych_12month +
                   mental_health_subj +
-                  inheritance, data = df_better
+                  inheritance, data = df
      ) #+ w1sq7_1 +w1sq7_2+w1sq7_3 +
 summary(model_cont)
 
@@ -146,12 +166,12 @@ model_cont_35 = lm(timetojob ~ cntry +
                   intern_paid +
                   psych_12month +
                   mental_health_subj +
-                  inheritance, data = df_better[df_better$agegroup35 == 1,]
+                  inheritance, data = df[df$agegroup35 == 1,]
      ) #+ w1sq7_1 +w1sq7_2+w1sq7_3 +
 summary(model_cont_35)
 
 ## categorical outcome ----
-df_better$timetojob_fac = factor(df_better$timetojob,
+df$timetojob_fac = factor(df$timetojob,
                       levels = c(1, 2, 3, 4),
                       labels = c("<1 month", "1–6 months", "6–12 months", ">1 year"),
                       ordered = TRUE)
@@ -166,7 +186,7 @@ model_cat = polr(timetojob_fac ~ cntry +
                   intern_unpaid * inheritance+
                   intern_paid * inheritance +
                   psych_12month +
-                  mental_health_subj, data = df_better, Hess = TRUE)
+                  mental_health_subj, data = df, Hess = TRUE)
 
 summary(model_cat)
 coefs = summary(model_cat)$coefficients
@@ -188,19 +208,64 @@ print(OR)
 # 1.Inheritance expectation → slower labour market entry: people with financial safety are less pressured to transition quickly
 # 2. Work experience matters less when financial safety is high: apprenticeship/internship effects are context-dependent
 # 3. Strongest pattern:  Labour market integration mechanisms (internships/apprenticeships) are more important for those without financial safety nets.
+
+# TODO:
+#' during_studies_... variablen ersetzen/ergänzen mit ob any(w1sq7_1, w1sq7_2, w1sq7_3)
+#' intern_paid1 und unpaid zu any zusammenführen
+#' apprent_unpaid1 und unpaid zsmführen
+#'
 # IDEEN:
 #' in das modell:
-#' w4sq1 - Importance of work in life --> einschätzung, ist es den leuten überhaupt wichtig?? --> das ist ein confounder (?)
 #' Ist die Person Ausländer
-#' Mental health --> allgemeine zufriedenheit im Leben
 #' alter --> evtl nur für unter 35? oder alter nicht linear einbeziehen
 
 # MAYBE ANALYSE MORE ON
 #' w3sq71 - Main obstacle in finding first job
 
 
+## categorical outcome simplified----
+df <- df %>%
+  mutate(
+    study_abroad = case_when(
+      w1sq6_6 == 1 ~ NA_real_,
+      during_studies_abroad_studies == 1 |
+      during_studies_abroad_work == 1 |
+      during_studies_abroad_langcourse == 1 |
+      during_studies_abroad_summerschool == 1 ~ 1,
+      w1sq6_5 == 1 ~ 0,
+      TRUE ~ NA_real_
+    ),
+    apprent = case_when(
+      apprent_unpaid == 1 | apprent_paid == 1 ~ 1,
+      apprent_unpaid == 0 & apprent_paid == 0 ~ 0,
+      TRUE ~ NA_real_
+    ),
+    intern = case_when(
+      intern_unpaid == 1 | intern_paid == 1 ~ 1,
+      intern_unpaid == 0 & intern_paid == 0 ~ 0,
+      TRUE ~ NA_real_
+    )
+  )
+model_cat_2 = polr(timetojob_fac ~ cntry + yrbrn0 +
+                  study_abroad +
+                  workimportance_subj +
+                  apprent* inheritance+
+                  intern * inheritance +
+                  psych_12month +
+                  mental_health_subj, data = df, Hess = TRUE)
+summary(model_cat_2)
+coefs = summary(model_cat_2)$coefficients
 
+p_values = 2 * pnorm(abs(coefs[, "t value"]), lower.tail = FALSE)
 
+cbind(coefs, p_value = p_values)
+p_values < 0.01
+
+coef_values = coefficients(model_cat_2)
+coef_values_all = c(coef_values[1:10], -1* sum(coef_values[1:10]), coef_values[11:length(coef_values)])
+names(coef_values_all)[1:11]  = paste0("cntry", levels(df$cntry))
+OR = exp(coef_values_all)
+print(OR)
 # Plots ----
 ## categories per country ----
 round(prop.table(table(df$w3sq69b, df$cntry), margin = 2), 2)
@@ -256,30 +321,30 @@ ggplot(df_plot, aes(x = age, y = prop, color = factor(w3sq69b))) +
 ## timetojob, country, mental health ----
 library(tidyr)
 
-df_better$timetojob <- factor(
-  df_better$timetojob,
+df$timetojob <- factor(
+  df$timetojob,
   ordered = TRUE
 )
 
 # prediction grid varyiing country and mental health
 newdata <- expand.grid(
-  cntry = levels(df_better$cntry),
-  mental_health_subj = sort(unique(df_better$mental_health_subj)),
+  cntry = levels(df$cntry),
+  mental_health_subj = sort(unique(df$mental_health_subj)),
 
-  during_studies_abroad_studies = levels(df_better$during_studies_abroad_studies)[1],
-  during_studies_abroad_work = levels(df_better$during_studies_abroad_work)[1],
-  during_studies_abroad_langcourse = levels(df_better$during_studies_abroad_langcourse)[1],
-  during_studies_abroad_summerschool = levels(df_better$during_studies_abroad_summerschool)[1],
+  during_studies_abroad_studies = levels(df$during_studies_abroad_studies)[1],
+  during_studies_abroad_work = levels(df$during_studies_abroad_work)[1],
+  during_studies_abroad_langcourse = levels(df$during_studies_abroad_langcourse)[1],
+  during_studies_abroad_summerschool = levels(df$during_studies_abroad_summerschool)[1],
 
-  apprent_unpaid = levels(df_better$apprent_unpaid)[1],
-  apprent_paid = levels(df_better$apprent_paid)[1],
-  intern_unpaid = levels(df_better$intern_unpaid)[1],
-  intern_paid = levels(df_better$intern_paid)[1],
+  apprent_unpaid = levels(df$apprent_unpaid)[1],
+  apprent_paid = levels(df$apprent_paid)[1],
+  intern_unpaid = levels(df$intern_unpaid)[1],
+  intern_paid = levels(df$intern_paid)[1],
 
-  psych_12month = levels(df_better$psych_12month)[1],
-  inheritance = levels(df_better$inheritance)[1],
+  psych_12month = levels(df$psych_12month)[1],
+  inheritance = levels(df$inheritance)[1],
 
-  workimportance_subj = mean(df_better$workimportance_subj, na.rm = TRUE)
+  workimportance_subj = mean(df$workimportance_subj, na.rm = TRUE)
 )
 
 # predicted probabilities
